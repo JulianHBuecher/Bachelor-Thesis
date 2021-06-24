82516429a0367b2cabcb5b3e35b228c91b57df1d
Julian Bücher <julian.buecher@gmx.de>
2021-06-24 15:41:30 +0200

    TF Random Forest Decision Tree für die Datasets GoldenEye, LOIC und Slowloris eingeführt

---
+++ ML.Proxy.ModelTrainer.Python/TensorFlow.DF/randomforest_GoldenEye.py
@@ -0,0 +1,87 @@
+import pandas as pd
+import sklearn as skl
+import tensorflow as tf
+import tensorflow_decision_forests as tfdf
+import numpy as np
+import os
+import tensorflow as tf
+import math
+from wurlitzer import sys_pipes
+import matplotlib.pyplot as plt
+
+from IPython.core.magic import register_line_magic
+from IPython.display import Javascript
+
+# Checking the version of installed TensorFlow Decision Forest
+print(f"Found TensorFlow Decision Forests v{tfdf.__version__}")
+
+data_GoldenEye = pd.read_csv('./Data/Thursday-15-02-2018_GoldenEye-Attack.csv')
+data_GoldenEye.rename(columns={
+    'Bwd Pkt Len Std':'bwd_pkt_len_std',
+    'Flow IAT Min':'flow_iat_min',
+    'Fwd IAT Min':'fwd_iat_min',
+    'Flow IAT Mean':'flow_iat_mean',
+    'Label':'label'
+},
+inplace=True)
+
+label = 'label'
+
+def split_dataset(dataset,  test_ratio=0.30):
+    """Splits a panda dataframe in two dataframes."""
+    test_indices = np.random.rand(len(dataset)) < test_ratio
+    return dataset[~test_indices], dataset[test_indices]
+
+training_data_GoldenEye, testing_data_GoldenEye = split_dataset(data_GoldenEye)
+
+print("{} examples in training, {} examples for testing.".format(
+    len(training_data_GoldenEye), len(testing_data_GoldenEye)))
+
+# Converting Panda Dataframe into Tensorflow Dataset
+print("Converting Panda Dataframe into TensorFlow Dataset: ")
+training_dataset_GoldenEye = tfdf.keras.pd_dataframe_to_tf_dataset(training_data_GoldenEye, label=label)
+testing_dataset_GoldenEye = tfdf.keras.pd_dataframe_to_tf_dataset(testing_data_GoldenEye, label=label)
+
+# Creating the Random Forest Model
+model = tfdf.keras.RandomForestModel()
+model.compile(metrics=["accuracy"])
+
+# Train the model
+print("Training the Model: ")
+with sys_pipes():
+    model.fit(x=training_dataset_GoldenEye)
+
+print("Evaluating the Model: ")
+evaluation = model.evaluate(testing_dataset_GoldenEye, return_dict=True)
+
+print()
+
+for name, value in evaluation.items():
+    print(f"{name}: {value:.4f}")
+
+# Saving the Model for later reusage
+model.save("../Models/goldeneye_model")
+
+# Plotting the first tree of the Decision Forest
+tfdf.model_plotter.plot_model_in_colab(model, tree_idx=0, max_depth=3)
+
+model.summary()
+
+# Showing the feature importances
+model.make_inspector().variable_importances()
+
+logs = model.make_inspector().training_logs()
+plt.figure(figsize=(12,4))
+
+plt.subplot(1,2,1)
+plt.plot([log.num_trees for log in logs], [log.evaluation.accuracy for log in logs])
+plt.xlabel("Number of trees")
+plt.ylabel("Accuracy (out-of-bag)")
+
+plt.subplot(1,2,2)
+plt.plot([log.num_trees for log in logs], [log.evaluation.loss for log in logs])
+plt.xlabel("Number of trees")
+plt.ylabel("Logloss (out-of-bag)")
+
+plt.savefig('../Data/Visualized/GoldenEye_Model.png',bbox_inches="tight")
+plt.clf()
--- ML.Proxy.ModelTrainer.Python/TensorFlow.DF/randomforest_LOIC.py
+++ ML.Proxy.ModelTrainer.Python/TensorFlow.DF/randomforest_LOIC.py
@@ -7,9 +7,12 @@ import os
 import tensorflow as tf
 import math
 from wurlitzer import sys_pipes
+import matplotlib.pyplot as plt
 
+from IPython.core.magic import register_line_magic
+from IPython.display import Javascript
 
-data_LOIC = pd.read_csv('./Data/Tuesday-20-02-2018_LOIC-Attack.csv')
+data_LOIC = pd.read_csv('../Data/Tuesday-20-02-2018_LOIC-Attack.csv')
 data_LOIC.rename(columns={
     'Bwd Pkt Len Std':'bwd_pkt_len_std',
     'Pkt Size Avg':'pkt_size_avg',
@@ -17,35 +20,6 @@ data_LOIC.rename(columns={
     'Flow IAT Std':'flow_iat_std',
     'Label':'label'}, 
     inplace=True)
-
-data_Slowloris = pd.read_csv('./Data/Thursday-15-02-2018_Slowloris-Attack.csv')
-data_Slowloris.rename(columns={
-    'Flow Duration':'flow_duration',
-    'Bwd IAT Mean':'bwd_iat_mean',
-    'Fwd IAT Min':'fwd_iat_min',
-    'Fwd IAT Mean':'fwd_iat_mean',
-    'Label':'label'
-},
-inplace=True)
-
-data_GoldenEye = pd.read_csv('./Data/Thursday-15-02-2018_GoldenEye-Attack.csv')
-data_GoldenEye.rename(columns={
-    'Bwd Pkt Len Std':'bwd_pkt_len_std',
-    'Flow IAT Min':'flow_iat_min',
-    'Fwd IAT Min':'fwd_iat_min',
-    'Flow IAT Mean':'flow_iat_mean',
-    'Label':'label'
-},
-inplace=True)
-
-# training_data_LOIC = data_LOIC.sample(frac=0.8, random_state=25)
-# testing_data_LOIC = data_LOIC.drop(training_data_LOIC.index)
-
-# training_data_Slowloris = data_Slowloris.sample(frac=0.8, random_state=25)
-# testing_data_Slowloris = data_Slowloris.drop(training_data_Slowloris.index)
-
-# training_data_GoldenEye = data_GoldenEye.sample(frac=0.8, random_state=25)
-# testing_data_GoldenEye = data_GoldenEye.drop(training_data_GoldenEye.index)
 
 label = 'label'
 
@@ -55,23 +29,25 @@ def split_dataset(dataset,  test_ratio=0.30):
     return dataset[~test_indices], dataset[test_indices]
 
 training_data_LOIC, testing_data_LOIC = split_dataset(data_LOIC)
-training_data_Slowloris, testing_data_Slowloris = split_dataset(data_Slowloris)
-training_data_GoldenEye, testing_data_GoldenEye = split_dataset(data_GoldenEye)
 
 print("{} examples in training, {} examples for testing.".format(
     len(training_data_LOIC), len(testing_data_LOIC)))
 
 # Converting Panda Dataframe into Tensorflow Dataset
+print("Converting Panda Dataframe into TensorFlow Dataset: ")
 training_dataset_LOIC = tfdf.keras.pd_dataframe_to_tf_dataset(training_data_LOIC, label=label)
 testing_dataset_LOIC = tfdf.keras.pd_dataframe_to_tf_dataset(testing_data_LOIC, label=label)
 
 # Creating the Random Forest Model
 model = tfdf.keras.RandomForestModel()
+model.compile(metrics=["accuracy"])
 
 # Train the model
+print("Training the Model: ")
 with sys_pipes():
     model.fit(x=training_dataset_LOIC)
 
+print("Evaluating the Model: ")
 evaluation = model.evaluate(testing_dataset_LOIC, return_dict=True)
 
 print()
@@ -79,6 +55,30 @@ print()
 for name, value in evaluation.items():
     print(f"{name}: {value:.4f}")
 
+
+# Saving the Model for later reusage
+model.save("../Models/loic_model")
+
+# Plotting the first tree of the Random Forest
 tfdf.model_plotter.plot_model_in_colab(model, tree_idx=0, max_depth=3)
 
 model.summary()
+
+# Showing the feature importances
+model.make_inspector().variable_importances()
+
+logs = model.make_inspector().training_logs()
+plt.figure(figsize=(12,4))
+
+plt.subplot(1,2,1)
+plt.plot([log.num_trees for log in logs], [log.evaluation.accuracy for log in logs])
+plt.xlabel("Number of trees")
+plt.ylabel("Accuracy (out-of-bag)")
+
+plt.subplot(1,2,2)
+plt.plot([log.num_trees for log in logs], [log.evaluation.loss for log in logs])
+plt.xlabel("Number of trees")
+plt.ylabel("Logloss (out-of-bag)")
+
+plt.savefig('../Data/Visualized/LOIC_Model.png',bbox_inches="tight")
+plt.clf()
