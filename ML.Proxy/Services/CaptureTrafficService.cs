4d31ef214d78094ae8a7735506138c0d7452b0ef
Julian Bücher <julian.buecher@gmx.de>
2021-07-15 16:05:31 +0200

    SharpPcap und PackageNET als Traffic Analyzer integriert

--- ML.Proxy/Services/CaptureTrafficService.cs
+++ ML.Proxy/Services/CaptureTrafficService.cs
@@ -1,11 +1,11 @@
 ﻿using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using PacketDotNet;
+using Serilog;
 using SharpPcap;
 using SharpPcap.LibPcap;
+using System;
+using System.IO;
+using System.Linq;
 
 namespace ML.Proxy.Services
 {
@@ -33,13 +33,56 @@ namespace ML.Proxy.Services
             else
             {
                 _logger.LogInformation($"{devices.Count} devices were found.");
+                foreach (var d in devices) { _logger.LogInformation($"Found Device Name: {d.Description}"); }
             }
 
-            // Here to continue (Error occure)
-            var device = devices
-                .Where(d => d.Interface.FriendlyName.Contains("Default Switch"))
-                .Select(d => d).First();
+            string networkInterface;
+            // Check der Laufzeitumgebung (Windows -> lokales Development)
+            if (OperatingSystem.IsWindows())
+            {
+                networkInterface = "Ethernet";
+            }
+            // Ansonsten Docker Container (Linux)
+            else
+            {
+                networkInterface = "eth0";
+            }
 
+            try
+            {
+                var device = devices
+                    .Where(d => d.Interface.FriendlyName != null 
+                        && d.Interface.FriendlyName.Equals(networkInterface))
+                    .Select(d => d)
+                    .First();
+
+                _logger.LogInformation($"Device for Capturing found: {device.Interface.FriendlyName}");
+
+                var readTimeoutMilliseconds = 500;
+
+                // Öffnen des Netzwerkinterfaces für das Abhören des Traffics
+                device.Open(new DeviceConfiguration { ReadTimeout = readTimeoutMilliseconds });
+
+                var rawCapture = device.GetNextPacket(out var p);
+
+                var time = p.Header.Timeval.Date;
+                var len = p.Data.Length;
+                var packet = p.GetPacket();
+
+                var processedPacket = Packet.ParsePacket(packet.LinkLayerType, packet.Data);
+
+                _logger.LogInformation($"{time.Hour}:{time.Minute}:{time.Second}:{time.Millisecond} Len={len}");
+                _logger.LogInformation(packet.ToString());
+
+                _logger.LogInformation(device.Statistics.ToString());
+
+                // Schließen des Netzwerkinterfaces
+                device.Close();
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($"Problem occured by using network interface: \n{e}");
+            }
         }
     }
 }
